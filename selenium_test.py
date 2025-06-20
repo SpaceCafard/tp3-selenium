caaf7cf2256fdeb4a2efc05beba83829de9b21f8
SpaceCafard <Mostefa69120@gmail.com>
2025-06-20 16:41:01 +0200

    comit selenium test

--- selenium_test.py
+++ selenium_test.py
@@ -4,19 +4,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 def test_login_selenium():
-    driver = webdriver.Chrome()  # Assurez-vous d'avoir ChromeDriver installé
+
+    driver = webdriver.Firefox(executable_path='C:/Users/SpaceCafard/Desktop/git_tp3/tp3-selenium/geckodriver.exe')
     driver.get("http://127.0.0.1:5000/")
-
-    # Trouver les champs de formulaire et les remplir
     username_field = driver.find_element(By.NAME, 'username')
-    password_field = driver.find_element(By.NAME, 'password')
-
+    password_field = driver.find_element(By.NAME, 'password')   
     username_field.send_keys('user')
     password_field.send_keys('password')
     password_field.send_keys(Keys.RETURN)
-
-    # Attendre que la page de tableau de bord se charge
     time.sleep(2)
-
     assert "Welcome to your dashboard!" in driver.page_source
     driver.quit()
